40566f2f55f2f33bfea82abc49322539782d2896
srivats88 <42479310+srivats88@users.noreply.github.com>
2020-09-29 08:55:21 -0700

    update_09292020
    Reworking of the DDX3 phylotree. MAFFT alignment and tree plotted using iTOL

--- DDX3X_3Y_analysis/msa_phylotree/ddx3_xy_msa_phylogenetic.R
+++ DDX3X_3Y_analysis/msa_phylotree/ddx3_xy_msa_phylogenetic.R
@@ -43,33 +43,3 @@ msaPrettyPrint(DDX3_XY_humanonly_refseq_protein_msa,
                verbose=FALSE,
                showConsensus = "none",
                paperWidth = 9)
-                          
-
-DDX3_XY_refseq_protein_subset_v3 <- readAAStringSet(filepath = "./DDX3_XY_refseq_protein_new_subset_v3.fasta", format = "fasta")
-DDX3_XY_refseq_protein_msa_subset_v3 <- msa(DDX3_XY_refseq_protein_subset_v3)
-
-DDX3_XY_refseq_protein_msa2_subset_v3 <- msaConvert(DDX3_XY_refseq_protein_msa_subset_v3, type="seqinr::alignment")
-DDX3_XY_refseq_protein_msa2_subset_v3_dist <- dist.alignment(DDX3_XY_refseq_protein_msa2_subset_v3, "identity")
-DDX3_XY_refseq_protein_subset_v3_tree <- nj(DDX3_XY_refseq_protein_msa2_subset_v3_dist)
-
-DDX3_XY_refseq_protein_subset_v3_tree_plot <-  ggtree(DDX3_XY_refseq_protein_subset_v3_tree) + 
-  geom_tiplab(size = 7) + 
-  xlim(0,0.35)
-
-pdf(file = "./DDX3_XY_refseq_protein_subset_v3_tree_plot.pdf", width = 18, height = 10)
-print(DDX3_XY_refseq_protein_subset_v3_tree_plot)
-dev.off()
-
-DDX3_XY_refseq_protein_v4 <- readAAStringSet(filepath = "./DDX3_XY_v4.fasta", format = "fasta")
-DDX3_XY_refseq_protein_v4_msa <- msa(DDX3_XY_refseq_protein_v4)
-
-DDX3_XY_refseq_protein_v4_msa2 <- msaConvert(DDX3_XY_refseq_protein_v4_msa, type="seqinr::alignment")
-DDX3_XY_refseq_protein_v4_msa2_dist <- dist.alignment(DDX3_XY_refseq_protein_v4_msa2, "identity")
-DDX3_XY_refseq_protein_v4_tree <- nj(DDX3_XY_refseq_protein_v4_msa2_dist)
-
-DDX3_XY_refseq_protein_v4_tree_plot <-  ggtree(DDX3_XY_refseq_protein_v4_tree) + 
-  geom_tiplab(size = 1.5) + xlim(0,0.3)
-
-pdf(file = "./DDX3_XY_refseq_protein_v4_tree_plot.pdf", width = 10, height = 10)
-print(DDX3_XY_refseq_protein_v4_tree_plot)
-dev.off()
